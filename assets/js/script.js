91c19f5587a73fd51b1087bb608a4d1ca177a809
Pam <pwillisop@yahoo.com>
2023-08-17 17:49:36 -0400

    trying to get to insert answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,33 @@ const questionContainer = document.getElementById("question-container");
 //heading
 const heading = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds!";
 //question-object
-let selectProblems = {
-  problem: {
+selectProblems = [
+  problem = {
     question: "Commonly used data types Do Not Include?",
     correctAnswer: "Alerts",
     answer: ['Strings', 'Booleans', 'Alerts', 'Numbers']
   },
-  problem: {
+  problem = {
     question: "Arrays in javascript can be used to store ______.",
     correctAnswer: "all of the above",
     answer: ['number of strings', 'other arrays', 'booleans', 'all of the above']
   },
-  problem: {
+  problem = {
     question: "String value must be enclosed within _______ when being assigned to variables",
     correctAnswer: "parenthesis",
     answer: ['commas', 'curly brackets', 'quotes', 'parenthesis']
   },
-  problem: {
+  problem = {
     question: "A very useful tool used during developement and debugging for printing content to the debugger is?",
     correctAnswer: ["console.log"],
     answer: ['Javascript', 'terminal/bash', 'for loops', 'console.log']
   },
-  problem: {
+  problem = {
     question: "The condition in an if / else statement enclosed within ________",
     correctAnswer: "curly brackets",
     answer: ['quotes', 'curly brackets', 'parenthesis', 'square brackets']
   }
-}
+]
 
 
 //Timer
@@ -73,22 +73,43 @@ function sendMessage() {
 //fill problem
 function startQuiz() {
   generate.classList.add("hide"); //hide start button
-  for (i = 0; i < selectProblems.problem.length; i++) { 
-    document.getElementById("question").add(selectProblems.problem.question);
-   } //fill answers
+  
+  for (i = 0; i < selectProblems.length; i++) {
+    console.log(selectProblems.length)
+    if (i<=selectProblems.length) {
+  document.getElementById("question").innerHTML = problem.question;
+  
+  document.getElementById("answerBtn")=document.createElement("li");
+
+    }
+  }
+}
+  
+  // selectProblems.problem.answer.forEach((answer) => console.log(answer));
+  // for(answer of selectProblems.problem.answer){
+  //   console.log(answer);
+  // }
+ 
+  // document.getElementById("grading").innerText = ("wrong");
+  
+
+  // for (i = 0; i <selectProblems.problem; i++) { 
+  //   console.log(selectProblems.problem);
+
+  //  } //fill answers
     // for (i = 0; i < selectProblems.problem.answer.length; i++) {
     //   selectProblems.problem.answer.forEach(i)
     //     document.createElement("ul");
     //   }
     // }
     //button click invokes comparison of click value and correct answer
-    if (selectProblems.problem.answer === selectProblems.problem.correctAnswer) {
-      document.getElementById("grading").innerText = ("correct");
-    }
-    else {
-      document.getElementById("grading").innerText = ("wrong");
-    }
-  }
+    // if (selectProblems.problem.answer === selectProblems.problem.correctAnswer) {
+    //   document.getElementById("grading").innerText = ("correct");
+    // }
+    // else {
+    //   document.getElementById("grading").innerText = ("wrong");
+    // }
+  
 
 
 generate.addEventListener("click", startQuiz)
