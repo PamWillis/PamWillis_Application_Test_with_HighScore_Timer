5d224b065c96b108f6da8bca966f732a4f7c7183
Pam <pwillisop@yahoo.com>
2023-08-19 22:11:17 -0400

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -163,7 +163,6 @@ function startQuiz() {
     }
   }
 
-
 }
 
 
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -4,3 +4,16 @@ function Score1() {
     document.querySelector(".score").textContent = Score;
   }
 Score1();
+var initials= [];
+
+
+
+function addInfo {
+  document.getElementsByTagName("label");
+  localStorage.setItem(initials, "value");
+}
+
+let myInitials = localStorage.getItem("initials");
+console.log(myInitals)
+
+submit.addEventListener("click", addInfo);
